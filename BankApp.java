7820b3e0d96d720f9382b1eefcaf01fa60a8bd7f
Suraj-behara <162140257+Suraj-behara@users.noreply.github.com>
2025-11-08 14:23:26 +0530

    Add files via upload

---
+++ BankApp.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+
+public class BankApp {
+    public static void main(String[] args) {
+        try {
+            // 1️⃣ Load JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 2️⃣ Connect to MySQL (update your password here)
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/bankdb", "root", "Suraj@sql1");
+
+            // 3️⃣ Insert a new record
+            String insertQuery = "INSERT INTO customers (name, balance) VALUES ('Suraj', 5000)";
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(insertQuery);
+            System.out.println(" Record Inserted!");
+
+            // 4️⃣ Display all customers
+            ResultSet rs = stmt.executeQuery("SELECT * FROM customers");
+
+           while (rs.next()) {
+                int acc_no = rs.getInt("acc_no");
+                String name = rs.getString("name");
+                double balance = rs.getDouble("balance");
+                 System.out.println(acc_no + " | " + name + " | Rs." + balance);
+            }
+
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
